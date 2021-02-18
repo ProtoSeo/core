b1b3b773744044f08312b7c27aac8b82817fe731
SeoSeunghun <proto_seo@naver.com>
2021-02-18 11:38:14 +0900

    관심사의 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,14 +3,20 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 이제 DIP를 준수하게 되었다. 완전하게 Interface에만 의존하게 되었다.
+    private final MemberRepository memberRepository;
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private DiscountPolicy discountPolicy;  // 이렇게 하면 DIP를 준수할 수 있다. 하지만 NullPointException 발생
+    private final DiscountPolicy discountPolicy;  // 이렇게 하면 DIP를 준수할 수 있다. 하지만 NullPointException 발생
                                             // 어떻게 해결? 누군가가 DiscountPolicy의 구현 객체를 대신 생성하고 주입해야한다.
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
