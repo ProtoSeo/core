fdb489934e5a606c7d3083bddfa2c5a1efe5a4ae
SeoSeunghun <proto_seo@naver.com>
2021-03-04 14:52:56 +0900

    @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,12 @@ public class ConfigurationSingletonTest {
         assertThat(memberRepository).isSameAs(memberRepository1);
         assertThat(memberRepository).isSameAs(memberRepository2);
     }
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean);
+    }
 }
