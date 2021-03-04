e7776321eab941559f675de925ca7fb78d0fd138
SeoSeunghun <proto_seo@naver.com>
2021-03-04 11:34:16 +0900

    싱글톤 방식의 주의점

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,51 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class StatefulServiceTest {
+
+    /*@Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //Thread A: A사용자 10000원 주문
+        statefulService1.order("userA",10000);
+        //Thread B: B사용자 20000원 주문
+        statefulService2.order("userB",20000);
+
+        //ThreadA: 사용자 A 주문금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }*/
+    @Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //Thread A: A사용자 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+        //Thread B: B사용자 20000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        //ThreadA: 사용자 A 주문금액 조회
+        System.out.println("price = " + userAPrice);
+
+        Assertions.assertThat(userAPrice).isEqualTo(10000);
+    }
+
+    static class TestConfig{
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+}
