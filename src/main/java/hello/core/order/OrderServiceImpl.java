895beadac2ed07fc66838b8e6d71d4a30cc5af4c
SeoSeunghun <proto_seo@naver.com>
2021-03-09 16:50:32 +0900

    롬복과 최신 트렌드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     // 이제 DIP를 준수하게 되었다. 완전하게 Interface에만 의존하게 되었다.
     private final MemberRepository memberRepository;
@@ -14,11 +16,12 @@ public class OrderServiceImpl implements OrderService{
     // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     private final DiscountPolicy discountPolicy;  // 이렇게 하면 DIP를 준수할 수 있다. 하지만 NullPointException 발생
                                             // 어떻게 해결? 누군가가 DiscountPolicy의 구현 객체를 대신 생성하고 주입해야한다.
-    @Autowired
+    /*@Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/
+
     /*
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
